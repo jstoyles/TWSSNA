5c628dcf87d7f0c19506ff89faddd4271a22b259
Jason Stoyles <jason.stoyles@gmail.com>
2018-01-16 12:50:51 -0500

    Removed error reporting

--- php/Classes/Purify.class.php
+++ php/Classes/Purify.class.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('display_errors', 'on');
-error_reporting(E_ALL);
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/php/htmlpurifier/library/HTMLPurifier.auto.php';
 $config = HTMLPurifier_Config::createDefault();
